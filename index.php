b97f87c9d0779249a88cffbcef4ff3bf48834bda
Pedro Salguero <pedrosalgueroanillo@gmail.com>
2021-01-20 18:01:38 +0100

    PHP_M2
    Ejercicio de la Tasca 2 , Nivell 1 - Exercici 1

---
+++ index.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="es" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <title>Tasca2</title>
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="description" content="Pagina de inicio para la tasca 2">
+        <meta name="keywords" content="html, css, php">
+        <link rel="stylesheet" href="css/style.css">
+    </head>
+
+    <body>
+
+        <header class="wrapper">
+            <div class="contenedor">
+                <div class="contenido logo">
+                    <a href="/"></a>
+                </div>
+                <div class="contenido redes-sociales">
+                    <ul>
+                        <li><a href="#"></a></li>
+                        <li><a href="#"></a></li>
+                        <li><a href="#"></a></li>
+                    </ul>
+
+                </div>
+            </div>
+            <!-- end contenedor header -->
+        </header>
+        <!-- Navegacion principal -->
+        <nav class=" principal wrapper">
+            <ul>
+                <li><a href="#"></a></li>
+                <li><a href="#"></a></li>
+                <li><a href="#"></a></li>
+                <li><a href="#"></a></li>
+                <li><a href="#"></a></li>
+            </ul>
+        </nav>
+
+        <!-- Secciones CTA  -->
+        <section class="cta wrapper">
+            <div class="contenedor">
+                <div class=""></div>
+                <div class=""></div>
+                <div class=""></div>
+            </div>
+        </section>
+
+        <!-- Contenido Principal -->
+        <main class="wrapper">
+            <aside>
+            </aside>
+
+            <section>
+            </section>
+
+            <aside>
+            </aside>
+        </main>
+        <!-- end Contenido Principal -->
+
+        <!-- Footer -->
+        <footer class="wrapper">
+
+        </footer>
+
+    </body>
+</html>
