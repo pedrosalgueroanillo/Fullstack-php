12becc153c351c271082e56d9ef69d5145194400
Pedro Salguero <pedrosalgueroanillo@gmail.com>
2021-01-22 16:14:10 +0100

    Ampliar estilos para responsive
    Añadir algunas modificaciones del responsive. Header principalmente

--- index.php
+++ index.php
@@ -143,7 +143,7 @@
                 <div>
                     <p>&copy;2021 - Todos los derechos reservados por El YETI</p>
                 </div>
-                <div>
+                <div class="datos-footer">
                     <a href="tel:333 666 999 "><i class="fas fa-phone-square-alt"> 333 666 999</i></a> - <a href="mailto:elyeti@tecomo.com"><i class="fas fa-envelope-square"></i> elyeti@tecomo.com </a>
                 </div>
         </footer>
