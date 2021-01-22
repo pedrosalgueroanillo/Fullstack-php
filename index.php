d27de7420088b68256726cb0e238905d0c210b10
Pedro Salguero <pedrosalgueroanillo@gmail.com>
2021-01-22 15:35:44 +0100

    Modificación de index.php
    Se añaden todos los elementos graficos del nuevo ejercicio. Modificación de estructura añadiendo clases y texto

--- index.php
+++ index.php
@@ -2,12 +2,15 @@
 <html lang="es" dir="ltr">
     <head>
         <meta charset="utf-8">
-        <title>Tasca2</title>
+        <title>Tasca2- Exercici 2</title>
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta name="description" content="Pagina de inicio para la tasca 2">
         <meta name="keywords" content="html, css, php">
         <link rel="stylesheet" href="css/style.css">
+        <link rel="stylesheet" href="fuentes/css/all.css">
+        <link rel="preconnect" href="https://fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com/css2?family=Rock+Salt&display=swap" rel="stylesheet">
     </head>
 
     <body>
@@ -15,56 +18,134 @@
         <header class="wrapper">
             <div class="contenedor">
                 <div class="contenido logo">
-                    <a href="/"></a>
+                    <a href="/"><img src="./imagenes/elyeti.png" alt="Yeti"></a>
                 </div>
                 <div class="contenido redes-sociales">
                     <ul>
-                        <li><a href="#"></a></li>
-                        <li><a href="#"></a></li>
-                        <li><a href="#"></a></li>
+                        <li><a class="fab fa-facebook" href="#"></a></li>
+                        <li><a class="fab fa-twitter" href="#"></a></li>
+                        <li><a class="fab fa-instagram" href="#"></a></li>
                     </ul>
 
                 </div>
             </div>
             <!-- end contenedor header -->
         </header>
-
         <!-- Navegacion principal -->
         <nav class=" principal wrapper">
             <ul>
-                <li><a href="#"></a></li>
-                <li><a href="#"></a></li>
-                <li><a href="#"></a></li>
-                <li><a href="#"></a></li>
-                <li><a href="#"></a></li>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Quien soy?</a></li>
+                <li><a href="#">BUHH !</a></li>
+                <li><a href="#">Noticias</a></li>
+                <li><a href="#">Contactar (Ironic mode on)</a></li>
             </ul>
         </nav>
 
         <!-- Secciones CTA  -->
         <section class="cta wrapper">
             <div class="contenedor">
-                <div class=""></div>
-                <div class=""></div>
-                <div class=""></div>
+                <div class="cta1" >
+
+                    <span class="bg-cta uno"></span>
+                    <h2>Mis amiguetes</h2>
+                    <a class="bton" href="#">Conócelos</a>
+                </div>
+
+                <div class="cta2">
+                    <span class="bg-cta dos"></span>
+                    <h2>Aquí veraneo</h2>
+                    <a class="bton" href="#">Ven conmigo</a>
+                </div>
+
+                <div class="cta3">
+                    <span class="bg-cta tres"></span>
+                    <h2>Mi dieta</h2>
+                    <a class="bton" href="#">Hipocalorias</a>
+                </div>
             </div>
         </section>
 
         <!-- Contenido Principal -->
         <main class="wrapper">
             <aside>
+                <h2>Todos mis amigos</h2>
+                <ul>
+                    <li><a href="">Faraonicos </a></li>
+                    <li><a href="">Halloweños </a></li>
+                    <li><a href="">Transilvania </a></li>
+                    <li><a href="">Lobitos </a></li>
+                    <li><a href="">Walkins </a></li>
+                    <li><a href="">Alienitas </a></li>
+                    <li><a href="">El nene </a></li>
+                </ul>
             </aside>
 
             <section>
+                <article>
+                    <div class="contenedor-noticias">
+                        <div class="imagen">
+                            <img src="imagenes/draculin.jpg" alt="draculin">
+                        </div>
+                        <div class="titulo">
+                            <h3><a href="#">Draculin</a></h1>
+                            <p>Este es mi mejor amgiguete en transilvania ...</p>
+                            <a href="#" class="leer-mas">Leer más</a>
+                        </div>
+                    </div>
+                </article>
+                <article>
+                    <div class="contenedor-noticias">
+                        <div class="imagen">
+                            <img src="imagenes/lobito.jpg" alt="lobito">
+                        </div>
+                        <div class="titulo">
+                            <h3><a href="#">Lobito</a></h1>
+                            <p>Este es mi amiguete peludo, es muy calentito ...</p>
+                            <a href="#" class="leer-mas">Leer más</a>
+                        </div>
+                    </div>
+                </article>
+                <article>
+                    <div class="contenedor-noticias">
+                        <div class="imagen">
+                            <img src="imagenes/franki.jpg" alt="franki">
+                        </div>
+                        <div class="titulo">
+                            <h3><a href="#">Tornillitos</a></h1>
+                            <p>Tornillitos es un machambrao de tio, tiene más piezas que ...</p>
+                            <a href="#" class="leer-mas">Leer más</a>
+                        </div>
+                    </div>
+                </article>
+
             </section>
 
             <aside>
+                <h2>Mis servicios</h2>
+                <ul>
+                    <li><a href="">Te asusto </a></li>
+                    <li><a href="">Te grito </a></li>
+                    <li><a href="">Me escondo </a></li>
+                    <li><a href="">Te mojo </a></li>
+                    <li><a href="">Te ensucio </a></li>
+                    <li><a href="">Te canto </a></li>
+                    <li><a href="">Te achucho </a></li>
+
+                </ul>
             </aside>
         </main>
         <!-- end Contenido Principal -->
 
         <!-- Footer -->
         <footer class="wrapper">
-            
+            <div class="contenedor">
+                <div>
+                    <p>&copy;2021 - Todos los derechos reservados por El YETI</p>
+                </div>
+                <div>
+                    <a href="tel:333 666 999 "><i class="fas fa-phone-square-alt"> 333 666 999</i></a> - <a href="mailto:elyeti@tecomo.com"><i class="fas fa-envelope-square"></i> elyeti@tecomo.com </a>
+                </div>
         </footer>
 
     </body>
