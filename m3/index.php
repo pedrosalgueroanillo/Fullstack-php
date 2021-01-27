c52543b08d11bb8f2a9196a5eda6ef15bf6c9ad0
Pedro Salguero <pedrosalgueroanillo@gmail.com>
2021-01-27 18:42:00 +0100

    cambiamos ubicacion archivo index
    Lo englobamos dentro de m3 . Hace referencia al ejercicio

---
+++ m3/index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="es" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <title>Tasca3</title>
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="description" content="Ejercicio 3">
+        <meta name="keywords" content="html, css, php">
+        <link rel="stylesheet" href="css/style-m3.css">
+        <link rel="preconnect" href="https://fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com/css2?family=Lato:wght@100&display=swap" rel="stylesheet">
+    </head>
+
+    <body>
+        <div class="contenedor">
+            <div class="block uno">
+                <div class="contenedor-texto">
+                    <h1 class="texto">IT</h1>
+                </div>
+            </div>
+
+            <div class="block dos">
+                <div class="contenedor-texto">
+                    <h1 class="texto">A</h1>
+                </div>
+            </div>
+
+             <div class="block tres">
+                <div class="contenedor-texto">
+                    <h1 class="texto">CA</h1>
+                </div>
+             </div>
+
+            <div class="block cuatro">
+                <div class="contenedor-texto">
+                    <h1 class="texto">DE</h1>
+                </div>
+             </div>
+
+            <div class="block cinco">
+                <div class="contenedor-texto">
+                    <h1 class="texto">MY &hearts;</h1>
+                </div>
+             </div>
+        </div><!-- end contenedor -->
+
+    </body>
+</html>
