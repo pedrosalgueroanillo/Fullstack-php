3be4d221384b770cb56e20e1ec113bb6fdeaa239
Pedro Salguero <pedrosalgueroanillo@gmail.com>
2021-01-28 15:46:48 +0100

    Correcion de bloques
    Corrección de los bloques

--- m3/index.php
+++ m3/index.php
@@ -2,11 +2,11 @@
 <html lang="es" dir="ltr">
     <head>
         <meta charset="utf-8">
-        <title>Tasca3</title>
+        <title>M3 - Tasca3</title>
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta name="description" content="Ejercicio 3">
-        <meta name="keywords" content="html, css, php">
+        <meta name="keywords" content="html, css">
         <link rel="stylesheet" href="css/style-m3.css">
         <link rel="preconnect" href="https://fonts.gstatic.com">
         <link href="https://fonts.googleapis.com/css2?family=Lato:wght@100&display=swap" rel="stylesheet">
@@ -14,34 +14,25 @@
 
     <body>
         <div class="contenedor">
+            <!--bolque 1 -->
             <div class="block uno">
-                <div class="contenedor-texto">
-                    <h1 class="texto">IT</h1>
-                </div>
+                <h1 class="texto">IT</h1>
             </div>
-
+            <!--bolque 2 -->
             <div class="block dos">
-                <div class="contenedor-texto">
-                    <h1 class="texto">A</h1>
-                </div>
+                <h1 class="texto">A</h1>
             </div>
-
-             <div class="block tres">
-                <div class="contenedor-texto">
-                    <h1 class="texto">CA</h1>
-                </div>
-             </div>
-
+            <!--bolque 3 -->
+            <div class="block tres">
+                <h1 class="texto">CA</h1>
+            </div>
+            <!--bolque 4 -->
             <div class="block cuatro">
-                <div class="contenedor-texto">
-                    <h1 class="texto">DE</h1>
-                </div>
-             </div>
-
+                <h1 class="texto">DE</h1>
+            </div>
+            <!--bolque 5 -->
             <div class="block cinco">
-                <div class="contenedor-texto">
-                    <h1 class="texto">MY &hearts;</h1>
-                </div>
+                <h1 class="texto">MY &hearts;</h1>
              </div>
         </div><!-- end contenedor -->
 
