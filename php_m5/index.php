5735b935d5b08b4c6e3b4dd1dfdef4ec59127ccd
Pedro Salguero <pedrosalgueroanillo@gmail.com>
2021-02-05 21:05:09 +0100

    Nivell 1 Spring 6

---
+++ php_m5/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        //EXERCICI 1
+        //Defineix una variable de cada tipus: integer,double,string i boolean. Després, imprimeix-les per pantalla.
+
+            $variable_integer   = 12;
+            $variable_double    = 12.20;
+            $variable_string    = "Ole"
+            $variable_boolean   = true;
+
+        // Las imprimimos en pantalla
+            echo $variable_integer . "<br>";
+            echo $variable_double . "<br>";
+            echo $variable_string . "<br>";
+            echo $variable_boolean . "<br>";
+
+
+
+
+
+                  ?>
+    </body>
+</html>
