e7880c928708e43d9daa4ad55df7e5ba5388643c
Pedro Salguero <pedrosalgueroanillo@gmail.com>
2021-02-08 17:48:21 +0100

    Creamos estructura

---
+++ php_m6/index.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sprint 6</title>
+    <style>
+    body {background-color: silver;}
+        .contenedor {width: 1000px; margin: 0 auto; padding: 20px; background-color: #fff; }
+        pre { border: 1px solid silver; padding: 1rem; background-color: #f1f1f1; width: 50%}
+    </style>
+</head>
+<body>
+    <div class="contenedor">
+        <h1>Sprint 6</h1>
+        <h2>Nivell 1</h2>
+        <h3>- Exercici 1</h3>
+        <p>Programa la funció "resta" que, donats dos paràmetres ens retorni la resta dels dos números.</p>
+
+
+        <h3>- Exercici 2</h3>
+        <p>Programa una lògica que, donat un número qualsevol(per exemple,la teva edat) ens digui si és parell o imparell mitjançant un missatge per pantalla.</p>
+
+
+        <h3>- Exercici 3</h3>
+        <p>Agafa la lògica de l'exercici 2 i encapsulala en una funció de nom parell_o_imparell. Invoca aquesta funció per a comprovar que funciona correctament.</p>
+
+
+        <h3>- Exercici 4</h3>
+        <p>Programa un bucle que compti fins a 10, mostrant cada  número per pantalla.</p>
+
+
+
+
+        <h2>Nivell 2</h2>
+        <h3>- Exercici 1</h3>
+        <p>Per jugar a "l'amagatall"  se'ns ha demanat un programa que compti fins a 10. Però la persona que comptarà és una mica tramposa y ho farà comptant de dos en dos. Crea una funció que compti fins a 10, de 2 en 2, mostrant cada número del compte per pantalla.</p>
+
+
+        <h3>- Exercici 2</h3>
+        <p>Imagina't que volem que conti fins a un número diferent de 10. Programa la funció per a que el final del compte estigui parametritzat.</p>
+
+
+        <h3>- Exercici 3</h3>
+        <p>Per a prevenir oblits al utilitzar la nostra meravellosa funció "amagatall" establirem un paràmetre per defecte a 10 en la funció que s'encarrega de fer aquest compte. </p>
+
+
+        <h2>Nivell 3</h2>
+        <h3>- Exercici 1</h3>
+        <p>Ens han demanat un llistat amb tots els anys on es van produir jocs olímpics desde 1960(Roma) inclós fins al 2016(Río de Janeiro,també inclós). Programa un bucle que mostri per pantalla els anys olímpics dins de l'interval esmentat.</p>
+
+
+
+        <h3>- Exercici 2</h3>
+        <p>Imagina que som a una botiga on es ven:</p>
+
+        <ul>
+            <li>Xocolata: 1 euro</li>
+            <li>Xiclets: 0.50 euros</li>
+            <li>Carmels: 1.50 euros</li>
+        </ul>
+        <p>Implementa un programa que permeti afegir càlculs a un total de compres d'aquest tipus. Per exemple, que si comprem:</p>
+
+        <p>2 xocolates, 1 de xiclets i 1 de carmels, tinguem un programa que permeti anar afegint els subtotals a un total, tal que així,</p>
+
+        <p>funció(2 xocolates) + funció(1 de xiclets) + funció(1 de carmels) = 2+0.5+1.5
+Sent per tant el total, 4.</p>
+
+        <h3>- Exercici 2</h3>
+        <p>La criba d'Eratóstenes és un algoritme pensat per a trobar nombres primers dins d'un interval donat. Basats en l'informació de l'enllaç adjunt, implementa la criba d'Eratóstenes dins d'una funció, de tal forma que poguem invocar la funció per a un número concret.</p>
+
+
+    </div>
+</body>
+</html>
